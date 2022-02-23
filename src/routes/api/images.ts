6f2ba81e76c7e94f4e8979bb3c71ba93654e9bf0
EngMohamedRaafat <muhammedraafat@outlook.com>
2022-02-23 08:28:27 +0200

    fix: Update typed params and return types of functions

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { existsSync, promises as fs } from 'fs';
 import path from 'path';
 
@@ -8,40 +8,44 @@ import image from '../../utils/image';
 
 const routes = express.Router();
 
-routes.get('/', validateRequest, async (req, res, next) => {
-	const filename = req.query['filename'] as string;
-	const width = Number(req.query['width'] as string);
-	const height = Number(req.query['height'] as string);
+routes.get(
+	'/',
+	validateRequest,
+	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+		const filename = req.query['filename'] as string;
+		const width = Number(req.query['width'] as string);
+		const height = Number(req.query['height'] as string);
 
-	// get full and thumbnail path
-	const imageDir = image.getPaths();
-	// check on the thumb directory existence
-	if (!existsSync(imageDir.thumbnail)) {
-		await fs.mkdir(imageDir.thumbnail);
-	}
-	// append filename to the full path
-	imageDir.full = path.join(imageDir.full, `${filename}.jpg`);
-
-	// check if the original exists or not in full directory
-	if (existsSync(imageDir.full)) {
-		// generate path of the resized image
-		imageDir.thumbnail = path.join(
-			imageDir.thumbnail,
-			image.generateThumbnailImageName(filename, width, height, 'jpg')
-		);
-		// check if the resized image is not exists in the thumb directory
+		// get full and thumbnail path
+		const imageDir = image.getPaths();
+		// check on the thumb directory existence
 		if (!existsSync(imageDir.thumbnail)) {
-			// resize and save the resulted image
-			await image.resize(imageDir.full, width, height, imageDir.thumbnail);
-			// set status to 201 (created)
-			res.status(201);
+			await fs.mkdir(imageDir.thumbnail);
 		}
-		// send the resized image to be displayed
-		res.sendFile(imageDir.thumbnail);
-	} else {
-		// throw exception if original image is not found
-		next(new ValidationError(`'${imageDir.full}' does not exist`));
+		// append filename to the full path
+		imageDir.full = path.join(imageDir.full, `${filename}.jpg`);
+
+		// check if the original exists or not in full directory
+		if (existsSync(imageDir.full)) {
+			// generate path of the resized image
+			imageDir.thumbnail = path.join(
+				imageDir.thumbnail,
+				image.generateThumbnailImageName(filename, width, height, 'jpg')
+			);
+			// check if the resized image is not exists in the thumb directory
+			if (!existsSync(imageDir.thumbnail)) {
+				// resize and save the resulted image
+				await image.resize(imageDir.full, width, height, imageDir.thumbnail);
+				// set status to 201 (created)
+				res.status(201);
+			}
+			// send the resized image to be displayed
+			res.sendFile(imageDir.thumbnail);
+		} else {
+			// throw exception if original image is not found
+			next(new ValidationError(`'${imageDir.full}' does not exist`));
+		}
 	}
-});
+);
 
 export default routes;
